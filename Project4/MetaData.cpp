73854fa534881f73d5acaa855692c38367f0eaf3
terrawilliams <terrawilliams@nevada.unr.edu>
2019-04-15 18:40:49 -0700

    Finished SJF Scheduling

--- Project4/MetaData.cpp
+++ Project4/MetaData.cpp
@@ -263,13 +263,7 @@ void MetaData::Sort(std::string schedule)
         int maxIO = 0;
         int index = 0;
 
-        int order[mData.size()];
         std::vector<Process> temp;
-
-        for(int i = 0; i < mData.size(); i++)
-        {
-            temp.push_back(mData[i]);
-        }
 
         for(int j = 0; j < mData.size(); j++)
         {
@@ -281,23 +275,48 @@ void MetaData::Sort(std::string schedule)
                 {
                     index = i;
                     maxIO = mData[i].numInputOutput;
-                    order[j] = index;
                 }
             }
 
+            temp.push_back(mData[index]);
             mData[index].numInputOutput = 0;
         }
 
         mData.clear();
         for(int i = 0; i < temp.size(); i++)
         {
-            mData.push_back(temp[order[i]]);
+            mData.push_back(temp[i]);
         }
     }
     else if(schedule == "SJF")
     {
         int minTasks = 0;
         int index = 0;
+
+        std::vector<Process> temp;
+
+        for(int j = 0; j < mData.size(); j++)
+        {
+            minTasks = mData[0].numTasks;
+
+            for(int i = 0; i < mData.size(); i++)
+            {
+                if(mData[i].numTasks < minTasks)
+                {
+                    index = i;
+                    minTasks = mData[i].numTasks;
+                }
+            }
+
+            temp.push_back(mData[index]);
+            mData[index].numTasks = 500;
+        }
+
+        mData.clear();
+        for(int i = 0; i < temp.size(); i++)
+        {
+            mData.push_back(temp[i]);
+        }
     }
     else
     {
